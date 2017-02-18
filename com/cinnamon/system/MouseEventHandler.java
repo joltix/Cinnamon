cfaaa80407a914b6934bf3e27fa6eb7412e847bf
Christian Ramos <chrisramos343@gmail.com>
2017-02-18 15:11:42 -0500

    Created wrapper for EventHandler<MouseEvent>

---
+++ com/cinnamon/system/MouseEventHandler.java
@@ -0,0 +1,8 @@
+package com.cinnamon.system;
+
+/**
+ * Wrapper for an {@link EventHandler} of type {@link MouseEvent}.
+ */
+public interface MouseEventHandler extends EventHandler<MouseEvent>
+{
+}
