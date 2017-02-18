ea2cb99faaa4aa7b9e8c1665fe20cf1d376c845f
Christian Ramos <chrisramos343@gmail.com>
2017-02-18 15:20:57 -0500

    Wrapper for an EventHandler<CreateEvent>

---
+++ com/cinnamon/system/CreateEventHandler.java
@@ -0,0 +1,7 @@
+package com.cinnamon.system;
+
+/**
+ * Wrapper for an {@link EventHandler} of type {@link CreateEvent}.
+ */
+public interface CreateEventHandler extends EventHandler<CreateEvent> {
+}
