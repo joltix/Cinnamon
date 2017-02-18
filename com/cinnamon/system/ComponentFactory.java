d9c0195173a2aa3ffbe2df56d9ff16aa765e75f9
Christian Ramos <chrisramos343@gmail.com>
2017-02-18 15:43:36 -0500

    Added Component access for subclasses, OnOrphanChangedListener support, and Component removal callback

--- com/cinnamon/system/ComponentFactory.java
+++ com/cinnamon/system/ComponentFactory.java
@@ -158,6 +158,17 @@ public abstract class ComponentFactory<E extends ComponentFactory.Component, U>
     protected abstract void onRequisition(E component);
 
     /**
+     * <p>Gets the {@link Component} with the given id.</p>
+     *
+     * @param id id.
+     * @return Component.
+     */
+    protected final E getComponent(int id)
+    {
+        return mLookup.get(id);
+    }
+
+    /**
      * <p>Removes a {@link Component} and puts it in an object pool for
      * reuse. The id associated with the Component may also be reused at the
      * factory's discretion.</p>
@@ -170,11 +181,22 @@ public abstract class ComponentFactory<E extends ComponentFactory.Component, U>
         // Remove from lookup
         final E component = mLookup.remove(id);
 
+        // Notify subclasses of removal
+        onRemove(component);
+
         // Move to pool to be used again later
         mCompPool.add(component);
 
         return component;
     }
+
+    /**
+     * <p>Notifies subclasses of a {@link Component}'s removal. The Component's state is as it was when removal was
+     * requested.</p>
+     *
+     * @param component Component.
+     */
+    protected abstract void onRemove(E component);
 
     /**
      * <p>Removes all {@link Component}s.</p>
@@ -238,6 +260,8 @@ public abstract class ComponentFactory<E extends ComponentFactory.Component, U>
          */
         public static final int NULL = -1;
 
+        private OnOrphanChangedListener mOnOrphanChangedListener;
+
         // Owning GObject's id and version
         private int mOwnerId;
         private int mOwnerVersion;
@@ -296,11 +320,36 @@ public abstract class ComponentFactory<E extends ComponentFactory.Component, U>
             if (object == null) {
                 mOwnerId = NULL;
                 mOwnerVersion = NULL;
+
+                // Notify listener of orphan status
+                if (mOnOrphanChangedListener != null) {
+                    mOnOrphanChangedListener.onOrphanChanged(mId, true);
+                }
+
             } else {
 
+                // Check if was already orphan before gaining new owner
+                final boolean gainedOwner = mOwnerId != Component.NULL && mOwnerVersion != Component.NULL;
+
+                // Update id and version to new
                 mOwnerId = object.getId();
                 mOwnerVersion = object.getVersion();
+
+                // Notify no longer an orphan after gaining an owner
+                if (gainedOwner && mOnOrphanChangedListener != null) {
+                    mOnOrphanChangedListener.onOrphanChanged(mId, false);
+                }
             }
+        }
+
+        /**
+         * <p>Sets an {@link OnOrphanChangedListener} to be notified of orphan status changes.</p>
+         *
+         * @param listener OnOrphanChangedListener.
+         */
+        public final void setOnOrphanChangedListener(OnOrphanChangedListener listener)
+        {
+            mOnOrphanChangedListener = listener;
         }
     }
 }
