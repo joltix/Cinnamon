919e571acacb10cefa654a0e6f814609e4f015f6
Christian Ramos <chrisramos343@gmail.com>
2017-04-01 19:38:16 -0400

    Changed from OnDrawVisibilityChangeListener

--- com/cinnamon/gfx/OnDrawOrderChangeListener.java
+++ com/cinnamon/gfx/OnDrawOrderChangeListener.java
@@ -2,17 +2,13 @@ package com.cinnamon.gfx;
 
 /**
  * <p>
- *     Listener to be called whenever the {@link ImageFactory}'s drawing data
- *     may need to be updated.
+ *     Listener to be called whenever the {@link ImageFactory}'s drawable data order may need to be updated.
  * </p>
- *
- *
  */
-public interface OnDrawVisibilityChangeListener
+public interface OnDrawOrderChangeListener
 {
     /**
-     * <p>Called when there's been a change that affects the
-     * {@link ImageFactory}'s drawing data.</p>
+     * <p>Called when there's been a change that affects the {@link ImageFactory}'s drawing order.</p>
      */
     void onChange();
 }
