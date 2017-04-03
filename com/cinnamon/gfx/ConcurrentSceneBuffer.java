789318238078c8aa85a37327791af266df5a14c9
Christian Ramos <chrisramos343@gmail.com>
2017-04-02 22:07:32 -0400

    getReadScene() no longer returns empty Scene
    Added ability for getReadScene() to return the previous Scene reset to be drawn again. This prevents an uncomfortable flickering effect when no new Scene is available to be drawn.

--- com/cinnamon/gfx/ConcurrentSceneBuffer.java
+++ com/cinnamon/gfx/ConcurrentSceneBuffer.java
@@ -4,9 +4,8 @@ import com.cinnamon.system.Game;
 
 /**
  * <p>
- *     ConcurrentSceneBuffer allows {@link Drawable} data to be safely
- *     passed from the game update {@link Thread} to the drawing Thread. Below
- *     is a simple use example from within {@link Game} and {@link Canvas}.
+ *     ConcurrentSceneBuffer allows {@link Drawable} data to be safely passed from the game's update {@link Thread} to
+ *     the drawing Thread. Below is a simple use example between {@link Game} and {@link Canvas}.
  * </p>
  *
  * <br>
@@ -42,12 +41,12 @@ import com.cinnamon.system.Game;
  *     public void draw(SceneBuffer buffer)
  *     {
  *         // Pull available Scene
- *         final Scene scene = buffer.getReadFrame();
+ *         final Scene<Batch<Drawable>> scene = buffer.getReadScene();
  *
  *         // Draw each Batch in Scene
  *         while (!scene.isEmpty()) {
  *
- *              final Batch batch = scene.poll();
+ *              final Batch<Drawable> batch = scene.poll();
  *
  *              // Draw operations
  *
@@ -56,40 +55,62 @@ import com.cinnamon.system.Game;
  *
  *     }
  * </pre>
- *
- *
- *
  */
-public class ConcurrentSceneBuffer extends Canvas.SceneBuffer<Batch<Drawable>>
+public final class ConcurrentSceneBuffer extends Canvas.SceneBuffer<Batch<Drawable>>
 {
+    // Number of Drawables that can be drawn in any given Scene
+    private static final int LOAD = 300;
+
     // Lock for draw data reads
-    private Object mUpdateLock = new Object();
+    private final Object mUpdateLock = new Object();
+
+    // Flag for checking if producer Thread has a Scene ready
+    private volatile boolean mNewSceneAvailable = false;
 
     // Scene to add drawable data to
-    private Scene mWriteScene = new PooledScene();
+    private Scene<Batch<Drawable>> mWriteScene = new BatchedScene(LOAD);
 
     // Buffer scene prevents blocking while writing and reading
-    private Scene mBufferScene = new PooledScene();
+    private Scene<Batch<Drawable>> mBufferScene = new BatchedScene(LOAD);
 
     // Scene to draw
-    private Scene mReadScene = new PooledScene();
+    private Scene<Batch<Drawable>> mReadScene = new BatchedScene(LOAD);
 
     @Override
     public Scene<Batch<Drawable>> getWriteScene()
     {
-        // Overwrite any existing Drawables
+        // Make all batches available for use
         mWriteScene.clear();
 
         return mWriteScene;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The returned {@link Scene} may be the previous reset to be drawn again. This occurs if no new Scene has
+     * yet been made available with {@link #flush()}.</p>
+     *
+     * @return next Scene to draw.
+     */
     @Override
     public Scene<Batch<Drawable>> getReadScene()
     {
+        // When no new Scene available, return previous reset
+        if (!mNewSceneAvailable) {
+            mReadScene.reset();
+            return mReadScene;
+        }
+
         synchronized (mUpdateLock) {
-            final Scene readScene = mReadScene;
+
+            // Rotate Scenes for reuse
+            final Scene<Batch<Drawable>> readScene = mReadScene;
             mReadScene = mBufferScene;
             mBufferScene = readScene;
+
+            // Flag scene as consumed
+            mNewSceneAvailable = false;
 
             return mReadScene;
         }
@@ -99,9 +120,12 @@ public class ConcurrentSceneBuffer extends Canvas.SceneBuffer<Batch<Drawable>>
     public void flush()
     {
         synchronized (mUpdateLock) {
-            final Scene oldScene = mWriteScene;
+            final Scene<Batch<Drawable>> oldScene = mWriteScene;
             mWriteScene = mBufferScene;
             mBufferScene = oldScene;
+
+            // Flag new scene for reading
+            mNewSceneAvailable = true;
         }
     }
 }
