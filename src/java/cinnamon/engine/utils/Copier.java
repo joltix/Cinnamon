881aaef739400f60d4eab21038c731ac4b7fdff8
Christian Ramos <chrisramos343@gmail.com>
2017-10-01 04:19:19 -0400

    Replaced teletype tags with @code and clarified copy()'s comments

--- src/java/cinnamon/engine/utils/Copier.java
+++ src/java/cinnamon/engine/utils/Copier.java
@@ -2,9 +2,9 @@ package cinnamon.engine.utils;
 
 /**
  * <p>Allows objects to copy the properties of another. Implementations are not required to duplicate the totality of a
- * given object's fields nor to subclass or be <tt>T</tt> itself.</p>
+ * given object's fields nor to subclass or be {@code T} itself.</p>
  *
- * <p>This is unlike <tt>clone()</tt> in that there is no expectation for a <tt>Copier</tt> that has copied an
+ * <p>This is unlike {@code clone()} in that there is no expectation for a {@code Copier} that has copied an
  * object to cause either of the following expressions to evaluate as true: <code>copier.equals(copiedObject)</code>
  * or <code>copier.getClass() == copiedObj.getClass()</code>. Furthermore, object creation is separated from copying
  * fields. The intent is not so much to create identical objects as it is to provide a more flexible and succinct
@@ -16,6 +16,10 @@ public interface Copier<T>
 {
     /**
      * <p>Copies properties of a given object.</p>
+     *
+     * <p>A {@code ClassCastException} may be thrown if the intended object class to copy is not the exact
+     * same as the class of the given object. This extends to super classes (e.g. if the declared target type is a
+     * sub class of the actual given object, implementations may throw the exception).</p>
      *
      * @param object to copy.
      * @throws ClassCastException if the given object's class does not match the intended copy target.
