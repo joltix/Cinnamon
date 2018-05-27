36a7354bff23209ae621773acd9b3d6ab8fa7e9f
Christian Ramos <chrisramos343@gmail.com>
2018-05-27 18:33:12 -0400

    Updated renamed reference

--- test/java/cinnamon/engine/event/IntegratableInputTestSuite.java
+++ test/java/cinnamon/engine/event/IntegratableInputTestSuite.java
@@ -2,9 +2,7 @@ package cinnamon.engine.event;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.system.MemoryUtil;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({IntegratableInputTest.class, IntegratableInputCallbackTest.class})
+@Suite.SuiteClasses({IntegratableInputTest.class, IntegratableInputCallbackExceptionsTest.class})
 public class IntegratableInputTestSuite { }
